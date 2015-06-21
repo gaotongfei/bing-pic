2d1c479e2120d79509894620766a3d63c526f3c2
gaotongfei <gaotongfei1995@gmail.com>
2015-06-21 15:57:35 +0800

    absoulte path

--- crawler.py
+++ crawler.py
@@ -1,5 +1,7 @@
+#!/home/gao/project/bingapi/venv/bin/python3
 # coding=utf-8
 import re
+import os
 import requests
 import shutil
 from bs4 import BeautifulSoup
@@ -9,7 +11,8 @@ soup = BeautifulSoup(response.text)
 pattern = re.compile('g_img={url:\'(.+?)\'')
 url = 'https://www.bing.com'+ pattern.findall(soup.text)[0]
 image_response = requests.get(url, stream=True)
-path = 'bing_image/image.jpg'
+path = os.path.split(os.path.realpath(__file__))[0] + '/bing_image/image.jpg'
+print(path)
 if image_response.status_code == 200:
     with open(path, 'w+b') as image:
         image_response.raw.decode_content = True
